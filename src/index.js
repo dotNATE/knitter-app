f410b7d7d6bde90028c2d03a0cb92330cff693d4
dotNate <somersetnathaniel@gmail.com>
2021-07-10 14:37:31 +0100

    set up graphql server and test query

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import { ApolloServer, gql } from "apollo-server-express";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs";
 
 const server = async () => {
     const app = express();
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
+
+    server.applyMiddleware({app});
 
     app.get('/', (req, res) => res.send('hello'));
 
